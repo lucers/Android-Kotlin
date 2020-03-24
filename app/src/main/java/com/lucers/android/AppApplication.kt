9677505c62070735d92e9bbe0cbf866a98392265
Lucers <tuhonglong@qq.com>
2020-03-24 21:45:45 +0800

    结构重组,kotlin插件更新

--- app/src/main/java/com/lucers/android/AppApplication.kt
+++ app/src/main/java/com/lucers/android/AppApplication.kt
@@ -1,32 +1,8 @@
 package com.lucers.android
 
-import com.lucers.common.DelegatesExt
 import com.lucers.common.base.BaseApplication
-import com.lucers.common.utils.LogUtil
-import com.tencent.mmkv.MMKV
 
 /**
  * AndroidApplication
  */
-class AppApplication : BaseApplication() {
-
-    companion object {
-        var INSTANCE: AppApplication by DelegatesExt.notNullSingleValue()
-
-        var mmkv: MMKV by DelegatesExt.notNullSingleValue()
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        INSTANCE = this
-
-        initMMKV()
-    }
-
-    private fun initMMKV() {
-        val mmkvPath = MMKV.initialize(this)
-        LogUtil.i("Lucers", "mmkvPath:$mmkvPath")
-
-        mmkv = MMKV.defaultMMKV()
-    }
-}
+class AppApplication : BaseApplication()
--- app/src/main/java/com/lucers/android/constants/AppRouteConstants.kt
+++ app/src/main/java/com/lucers/android/constants/AppRouteConstants.kt
@@ -1,4 +1,6 @@
-package com.huatian.dididiangong.constants
+package com.lucers.android.constants
+
+import com.lucers.android.BuildConfig
 
 /**
  * AppRouteConstants
@@ -6,6 +8,17 @@ package com.huatian.dididiangong.constants
 class AppRouteConstants {
 
     companion object {
-        const val routeRoot: String = "/app"
+        private const val routeRoot: String = BuildConfig.ROUTE_ROOT
+
+        const val splashRoute: String = "$routeRoot/splash"
+        const val mainRoute: String = "$routeRoot/main"
+        const val loginRoute: String = "$routeRoot/login"
+        const val settingRoute: String = "$routeRoot/setting"
+        const val profileRoute: String = "$routeRoot/profile"
+        const val browseRecordsRoute: String = "$routeRoot/browse_records"
+        const val feedbackRoute: String = "$routeRoot/feedback"
+        const val websRoute: String = "$routeRoot/webs"
+        const val aboutUsRoute: String = "$routeRoot/about_us"
+        const val helpCenterRoute: String = "$routeRoot/help_center"
     }
 }
