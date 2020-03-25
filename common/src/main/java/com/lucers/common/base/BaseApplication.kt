db358351ec90e068302c9f15234fb52d68b8dff0
Lucers <tuhonglong@qq.com>
2020-03-25 23:15:17 +0800

    change app module

--- common/src/main/java/com/lucers/common/base/BaseApplication.kt
+++ common/src/main/java/com/lucers/common/base/BaseApplication.kt
@@ -3,8 +3,6 @@ package com.lucers.common.base
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
-import com.airbnb.mvrx.MvRx
-import com.airbnb.mvrx.MvRxViewModelConfigFactory
 import com.alibaba.android.arouter.launcher.ARouter
 import me.jessyan.autosize.AutoSizeConfig
 
